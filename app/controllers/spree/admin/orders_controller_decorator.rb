a7545a93017877d9b00096780481e0c981a6c20d
Paweł Łuczak <woochaq@gmail.com>
2014-01-04 18:12:18 +0100

    Change exported values, export ordered products

--- app/controllers/spree/admin/orders_controller_decorator.rb
+++ app/controllers/spree/admin/orders_controller_decorator.rb
@@ -3,7 +3,11 @@ Spree::Admin::OrdersController.class_eval do
     created_at_gt = Time.zone.parse(params[:created_at_gt]).beginning_of_day rescue ""
     created_at_lt = Time.zone.parse(params[:created_at_lt]).end_of_day rescue ""
 
-    @orders = Spree::Order.paid_and_ready_to_ship.completed_between(created_at_gt, created_at_lt).order("created_at")
+    if created_at_gt.present? and created_at_lt.present?
+      @orders = Spree::Order.paid_and_ready_to_ship.completed_between(created_at_gt, created_at_lt).order("created_at")
+    else
+      @orders = Spree::Order.order("created_at")
+    end
     send_data @orders.export_csv, filename: "orders.csv"
   end
 end
--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -9,17 +9,46 @@ Spree::Order.instance_eval do
     where(payment_state: 'paid', shipment_state: 'ready')
   end
 
+
   def self.export_csv(options = {})
     CSV.generate(options) do |csv|
-      address_column_names = ["firstname", "lastname", "address1", "address2", "city", "zipcode", "phone", "state_name", "alternative_phone", "company"]
-      order_column_names = ["id", "number", "total", "state", "shipment_state", "email", "user_id", "completed_at", "payment_state", "special_instructions"]
-      column_headers = order_column_names + address_column_names
+      address_column_names = ["address1", "firstname", "lastname", "zipcode", "city", "phone"]
+      order_column_names = ["number", "email", "created_at", "total"]
+      products_column_names = ["products", "product count"]
+      billing_column_names = ["address1", "firstname", "lastname", "zipcode", "city", "phone"]
+      column_headers = order_column_names + products_column_names + address_column_names + billing_column_names
       csv << column_headers
       all.each do |order|
-        values = order.attributes.values_at(*order_column_names) + order.ship_address.attributes.values_at(*address_column_names)
+        values = order.attributes.values_at(*order_column_names) + products(order) + products_quantity(order) +
+          ship_address_values(order, address_column_names) + bill_values(order, billing_column_names)
         csv << values
       end
     end
   end
+
+  private
+    def products(order)
+      [order.products.map{ |p| p.name }.join(',')]
+    end
+
+    def products_quantity(order)
+      [order.line_items.map{ |p| p.quantity }.join(',')]
+    end
+
+    def ship_address_values(order, address_column_names)
+      if order.ship_address.present?
+        order.ship_address.attributes.values_at(*address_column_names)
+      else
+        address_column_names.map{|i| ""}
+      end
+    end
+
+    def bill_values(order, billing_column_names)
+      if order.bill_address.present?
+        order.bill_address.attributes.values_at(*billing_column_names)
+      else
+        billing_column_names.map{|i| ""}
+      end
+    end
 
 end
